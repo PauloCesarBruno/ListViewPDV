2c86404b0116a470d7b0d774b9719adba5eb30d6
Paulo Bruno <p_bruno001@hotmail.com>
2021-05-18 16:43:44 -0300

    C#

--- DTO/Properties/Settings.Designer.cs
+++ DTO/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DTO.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace DTO.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-19BHTV0U;Initial Catalog=PDV;User ID=sa;Password=Paradoxo22")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-EJP79KA;Initial Catalog=PDV;User ID=sa;Password=Paradoxo22")]
         public string String {
             get {
                 return ((string)(this["String"]));
